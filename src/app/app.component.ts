d71a8e4144ca2512a7053b727d5345c5269c501d
Thomas Renger <thomas.renger@gmail.com>
2022-01-01 22:54:56 +0100

    feat: move the current implementation to this repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,117 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { interval } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+interface LocatedObject {
+  x: number,
+  y: number,
+  update: () => void,
+  collidate: (item: LocatedObject) => boolean
+}
+
+class RenderObject implements LocatedObject {
+  destroyed = false;
+
+  x: number;
+  y: number;
+
+  private readonly width = 20;
+  private readonly height = 20;
+
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+
+  update(): void {
+    this.y += 0.25;
+  }
+
+  collidate(item: LocatedObject): boolean {
+    const left = this.x - item.x;
+    const top = this.y - item.y;
+    return left >= -20 && left <= 20
+      && top >= -20 && top <= 20;
+  }
+}
+
+class ShotObject implements LocatedObject {
+  destroyed = false;
+
+  x: number;
+  y: number;
+
+  private readonly width = 1;
+  private readonly height = 1;
+
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+
+  update(): void {
+    this.y -= 2;
+  }
+
+  collidate(item: LocatedObject): boolean {
+    return (item.x <= this.x && (item.x - 20) >= this.x)
+      && (item.y <= this.y && (item.y - 20) >= this.y);
+  }
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'solar-striker';
+
+  player = new RenderObject(200, 800);
+  enemies: RenderObject[] = [];
+  shots: ShotObject[] = [];
+  deaths = 0;
+  kills = 0;
+
+  constructor() {
+    interval(5).pipe(
+      tap(value => {
+        this.crash();
+        if (value % 200 === 0) {
+          this.enemies.push(new RenderObject(value % 300, 0));
+        }
+        this.enemies.forEach(enemy => enemy.update());
+        this.shots.forEach(shot => shot.update());
+      }),
+    ).subscribe();
+  }
+
+  @HostListener('mousemove', ['$event'])
+  private move(event: MouseEvent): void {
+    this.player.x = event.clientX;
+  }
+
+  @HostListener('click')
+  private shot(): void {
+    this.shots.push(new ShotObject(this.player.x, this.player.y));
+    this.shots.push(new ShotObject(this.player.x + 20, this.player.y));
+  }
+
+  private crash(): void {
+    this.shots.forEach(shot => {
+      const enemy = this.enemies.find(enemy => enemy.collidate(shot));
+      if (enemy) {
+        enemy.destroyed = true;
+        shot.destroyed = true;
+        this.kills++;
+      }
+    });
+    this.shots = this.shots.filter(shot => !shot.destroyed && (shot.y > 0));
+    const enemy = this.enemies.find(enemy => !enemy.destroyed && enemy.collidate(this.player));
+    if (enemy) {
+      enemy.destroyed = true;
+      this.deaths++;
+    }
+    this.enemies = this.enemies.filter(enemy => !enemy.destroyed && enemy.y < 2000);
+  }
 }
