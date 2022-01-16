47ca4e5f3d495b28a961dcf8c52a1338a46c58a6
Thomas Renger <thomas.renger@gmail.com>
2022-01-16 18:47:00 +0100

    feat: capacitor ios

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'net.bolzplatzarena.solarstriker',
+  appName: 'Solar Striker',
+  webDir: 'dist/solar-striker',
+  bundledWebRuntime: false,
+};
+
+export default config;
