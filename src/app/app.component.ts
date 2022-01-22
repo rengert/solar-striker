58cae7dfad81319dea1dad89e5f6b543dd91c813
Thomas Renger <thomas.renger@gmail.com>
2022-01-22 10:20:51 +0100

    feat: capacitor ios and the icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SplashScreen } from '@capacitor/splash-screen';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    void SplashScreen.hide();
+  }
 }
