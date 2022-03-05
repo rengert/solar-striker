ef31694c014b1b31d1195014b6fe06bd8102c046
Thomas Renger <thomas.renger@gmail.com>
2022-03-05 13:47:27 +0100

    chore: remove not used code

--- src/app/models/pixijs/background-sprite.ts
+++ src/app/models/pixijs/background-sprite.ts
@@ -1,4 +1,4 @@
-import { Sprite, Texture, TilingSprite } from 'pixi.js';
+import { Texture, TilingSprite } from 'pixi.js';
 
 export class BackgroundSprite extends TilingSprite {
   constructor(
@@ -23,15 +23,5 @@ export class BackgroundSprite extends TilingSprite {
     if (this.y > this.maxY) {
       this.y = -this.height;
     }
-  }
-
-  hit(object2: Sprite): boolean {
-    const bounds1 = this.getBounds();
-    const bounds2 = object2.getBounds();
-
-    return bounds1.x < bounds2.x + bounds2.width
-      && bounds1.x + bounds1.width > bounds2.x
-      && bounds1.y < bounds2.y + bounds2.height
-      && bounds1.y + bounds1.height > bounds2.y;
   }
 }
