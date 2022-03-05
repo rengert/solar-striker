28d60e6ae65f8fc9906b82223a9ab5c4f659f016
Thomas Renger <thomas.renger@gmail.com>
2022-03-05 14:22:22 +0100

    fix: performance tweaks

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -45,8 +45,11 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js';  // Included with Angular CLI.
+import 'zone.js'; // Included with Angular CLI.
 
+(window as any).__Zone_disable_requestAnimationFrame = true; // disable patch requestAnimationFrame
+(window as any).__Zone_disable_on_property = true; // disable patch onProperty such as onclick
+(window as any).__zone_symbol__UNPATCHED_EVENTS = ['scroll', 'mousemove'];
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
