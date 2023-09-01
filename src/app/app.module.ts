64451fcf2ff3b8b100977a22f12b1cb864983160
Thomas Renger <thomas.renger@gmail.com>
2023-09-01 20:50:50 +0200

    chore: remove not used stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { HtmlComponent } from './components/html/html.component';
 import { PixijsComponent } from './components/pixijs/pixijs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HtmlComponent,
-    PixijsComponent
+    PixijsComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
