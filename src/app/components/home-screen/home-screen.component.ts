d1f672a3f9af8887f552450aa09ef31361a9dfb6
Thomas Renger <thomas.renger@gmail.com>
2023-09-02 13:05:15 +0200

    chore: return to onscreen only

--- src/app/components/home-screen/home-screen.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  templateUrl: './home-screen.component.html',
-  styleUrls: ['home-screen.component.scss'],
-})
-export class HomeScreenComponent {
-  readonly res = 'sss';
-}
--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ if (environment.production) {
 }
 
 platformBrowserDynamic()
-  .bootstrapModule(AppModule)
+  .bootstrapModule(AppModule, { ngZone: 'noop' })
   // eslint-disable-next-line no-console
   .catch(err => console.error(err));
