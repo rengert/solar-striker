3e5958b38970f2488aaa3a172590588f78446852
Thomas Renger <thomas.renger@gmail.com>
2023-09-19 21:24:34 +0200

    refactor: to use a base screen class

--- src/app/popups/credits-popup.ts
+++ src/app/popups/credits-popup.ts
@@ -1,39 +1,11 @@
 import { Button } from '@pixi/ui';
-import gsap from 'gsap';
-import { Container, Sprite, Text, Texture } from 'pixi.js';
+import { Sprite, Text } from 'pixi.js';
 import { PixiGameService } from '../services/pixi-game.service';
+import { Popup } from './popup';
 
-export class CreditsPopup extends Container {
-  private readonly background: Sprite;
-  private readonly panel: Sprite;
-  private readonly title: Text;
-  private readonly panelBase: Container;
-
+export class CreditsPopup extends Popup {
   constructor(private readonly gameService: PixiGameService) {
-    super();
-
-    this.background = Sprite.from(Texture.WHITE);
-    this.background.tint = 0xffcc55;
-    this.background.eventMode = 'none';
-    this.addChild(this.background);
-
-    this.panel = Sprite.from('assets/ui/navigation-popup.png');
-    this.panel.anchor.set(0.5);
-    this.panel.width = 265;
-    this.panel.height = 230;
-    this.addChild(this.panel);
-
-    this.panelBase = new Container();
-    this.panelBase.height = 400;
-    this.panelBase.width = 400;
-    this.panel.addChild(this.panelBase);
-
-    this.title = new Text('Credits', { fontFamily: 'DefaultFont', dropShadowColor: '000000', fontSize: 14 });
-    this.title.x = 0;
-    this.title.y = -96;
-    this.title.anchor.set(0.5, 0.5);
-    this.panel.addChild(this.title);
-
+    super('Credits');
 
     this.addText(this.panel, 'Idee & Programmierung', 14, -60);
     this.addText(this.panel, 'Thomas Renger', 12, -40);
@@ -41,29 +13,6 @@ export class CreditsPopup extends Container {
     this.addText(this.panel, 'Kenney (www.kenney.nl)', 12, 10);
 
     this.addCloseButton(this.panel);
-  }
-
-  public async show() {
-    gsap.killTweensOf(this.background);
-    gsap.killTweensOf(this.panel.pivot);
-    this.background.alpha = 0;
-    this.panel.pivot.y = -400;
-    gsap.to(this.background, { alpha: 0.8, duration: 0.2, ease: 'linear' });
-    await gsap.to(this.panel.pivot, { y: 0, duration: 0.3, ease: 'back.out' });
-  }
-
-  public async hide() {
-    gsap.killTweensOf(this.background);
-    gsap.killTweensOf(this.panel.pivot);
-    gsap.to(this.background, { alpha: 0, duration: 0.2, ease: 'linear' });
-    await gsap.to(this.panel.pivot, { y: -500, duration: 0.3, ease: 'back.in' });
-  }
-
-  public resize(width: number, height: number) {
-    this.background.width = width;
-    this.background.height = height;
-    this.panel.x = width * 0.5;
-    this.panel.y = height * 0.5;
   }
 
   private addCloseButton(panel: Sprite) {
@@ -79,17 +28,5 @@ export class CreditsPopup extends Container {
     creditButton.view.addChild(text);
     creditButton.onPress.connect(() => this.gameService.navigation(this));
     panel.addChild(creditButton.view);
-  }
-
-  addText(panel: Sprite, content: string, size: number, positionY: number): void {
-    const text = new Text(content, {
-      fontFamily: 'DefaultFont',
-      dropShadowColor: '000000',
-      fontSize: size,
-    });
-    text.x = 0;
-    text.y = positionY;
-    text.anchor.set(0.5, 0.5);
-    panel.addChild(text);
   }
 }
---
+++ src/app/popups/popup.ts
@@ -0,0 +1,77 @@
+import gsap from 'gsap';
+import { Container, Sprite, Text, Texture } from 'pixi.js';
+
+export abstract class Popup extends Container {
+  protected readonly panel: Sprite;
+
+  private readonly background: Sprite;
+  private readonly title: Text;
+  private readonly panelBase: Container;
+
+  constructor(title: string) {
+    super();
+
+    this.background = Sprite.from(Texture.WHITE);
+    this.background.tint = 0xffcc55;
+    this.background.eventMode = 'none';
+    this.addChild(this.background);
+
+    this.panel = Sprite.from('assets/ui/navigation-popup.png');
+    this.panel.anchor.set(0.5);
+    this.panel.width = 265;
+    this.panel.height = 230;
+    this.addChild(this.panel);
+
+    this.panelBase = new Container();
+    this.panelBase.height = 400;
+    this.panelBase.width = 400;
+    this.panel.addChild(this.panelBase);
+
+    this.title = new Text(title, { fontFamily: 'DefaultFont', dropShadowColor: '000000', fontSize: 14 });
+    this.title.x = 0;
+    this.title.y = -96;
+    this.title.anchor.set(0.5, 0.5);
+    this.panel.addChild(this.title);
+
+
+    this.addText(this.panel, 'Idee & Programmierung', 14, -60);
+    this.addText(this.panel, 'Thomas Renger', 12, -40);
+    this.addText(this.panel, 'Grafiken', 14, -10);
+    this.addText(this.panel, 'Kenney (www.kenney.nl)', 12, 10);
+  }
+
+  async show() {
+    gsap.killTweensOf(this.background);
+    gsap.killTweensOf(this.panel.pivot);
+    this.background.alpha = 0;
+    this.panel.pivot.y = -400;
+    gsap.to(this.background, { alpha: 0.8, duration: 0.2, ease: 'linear' });
+    await gsap.to(this.panel.pivot, { y: 0, duration: 0.3, ease: 'back.out' });
+  }
+
+  async hide() {
+    gsap.killTweensOf(this.background);
+    gsap.killTweensOf(this.panel.pivot);
+    gsap.to(this.background, { alpha: 0, duration: 0.2, ease: 'linear' });
+    await gsap.to(this.panel.pivot, { y: -500, duration: 0.3, ease: 'back.in' });
+  }
+
+  resize(width: number, height: number) {
+    this.background.width = width;
+    this.background.height = height;
+    this.panel.x = width * 0.5;
+    this.panel.y = height * 0.5;
+  }
+
+  protected addText(panel: Sprite, content: string, size: number, positionY: number): void {
+    const text = new Text(content, {
+      fontFamily: 'DefaultFont',
+      dropShadowColor: '000000',
+      fontSize: size,
+    });
+    text.x = 0;
+    text.y = positionY;
+    text.anchor.set(0.5, 0.5);
+    panel.addChild(text);
+  }
+}
