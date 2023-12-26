f907b800c5e539909d038c99b29e0f676ce1551c
Thomas Renger <info@bolzplatzarena.net>
2023-12-26 10:21:43 +0000

    Auto-generated commit [ci skip]
    Triggered by commit: https://github.com/rengert/solar-striker/commit/159ed8074209a9c06515d9c62816a196fbe1ae0c

---
+++ browser/chunk-JMLIMAZC.js
@@ -0,0 +1,1 @@
+import{h as n,j as e}from"./chunk-DYKBKFKR.js";var o=class extends e{show(i){return n(this,null,function*(){})}hide(i){return n(this,null,function*(){})}};export{o as SplashScreenWeb};
