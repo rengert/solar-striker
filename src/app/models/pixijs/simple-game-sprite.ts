f8eb90c4c146b61bd1cb80a2b6ca54f9a6bc46a2
Thomas Renger <thomas.renger@gmail.com>
2024-01-04 18:59:26 +0100

    feat: more random in the setup

--- src/app/models/pixijs/simple-game-sprite.ts
+++ src/app/models/pixijs/simple-game-sprite.ts
@@ -2,17 +2,20 @@ import { Sprite, Texture } from 'pixi.js';
 import { hit } from '../../utils/sprite.util';
 
 export class GameSprite extends Sprite {
-  private readonly speed: number = 1;
+  private readonly ySpeed: number;
+  private readonly xSpeed: number;
 
   constructor(speed: number, texture: Texture) {
     super(texture);
 
-    this.speed = speed;
+    this.ySpeed = Math.random() * speed;
+    this.xSpeed = Math.random() * speed / 2;
   }
 
   update(delta: number): void {
     this.rotation += 0.015 * delta;
-    this.y += delta * this.speed;
+    this.y += delta * this.ySpeed;
+    this.x += delta * this.xSpeed;
   }
 
   hit(object2: Sprite): boolean {
