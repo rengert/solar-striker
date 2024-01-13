c47d00120e0164e156482551554fd8e0075d4100
Thomas Renger <thomas.renger@gmail.com>
2024-01-13 11:30:19 +0100

    feat: support different sizes for the popup

--- src/app/popups/credits-popup.ts
+++ src/app/popups/credits-popup.ts
@@ -5,11 +5,11 @@ export class CreditsPopup extends Popup {
   constructor(gameService: GameService) {
     super('Credits');
 
-    this.addText(this.panel, 'Idee & Programmierung', { size: 14 }, { y: -60 });
-    this.addText(this.panel, 'Thomas Renger', { size: 12 }, { y: -40 });
-    this.addText(this.panel, 'Grafiken', { size: 14 }, { y: -10 });
-    this.addText(this.panel, 'Kenney (www.kenney.nl)', { size: 12 }, { y: 10 });
+    this.addText('Idee & Programmierung', { size: 14 }, { y: -60 });
+    this.addText('Thomas Renger', { size: 12 }, { y: -40 });
+    this.addText('Grafiken', { size: 14 }, { y: -10 });
+    this.addText('Kenney (www.kenney.nl)', { size: 12 }, { y: 10 });
 
-    this.addButton('Schließen!', () => gameService.openNavigation(this), 2, this.panel);
+    this.addButton('Schließen!', () => gameService.openNavigation(this), 2);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-
 bootstrapApplication(AppComponent)
   // eslint-disable-next-line no-console
   .catch(err => console.error(err));
