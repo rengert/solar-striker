a4abd17e1718dcbd1ca6decbb42b023e96aa0658
Thomas Renger <thomas.renger@gmail.com>
2024-08-16 21:59:50 +0200

    fix: speed of landscape

--- src/app/services/game-landscape.service.ts
+++ src/app/services/game-landscape.service.ts
@@ -1,64 +1,54 @@
-import {inject, Injectable} from '@angular/core';
-import {Texture, Ticker} from 'pixi.js';
-import {BackgroundSprite} from '../models/pixijs/background-sprite';
-import {ApplicationService} from './application.service';
-import {UpdatableService} from "./updatable.service";
+import { inject, Injectable } from '@angular/core';
+import { Texture, Ticker } from 'pixi.js';
+import { BackgroundSprite } from '../models/pixijs/background-sprite';
+import { ApplicationService } from './application.service';
+import { UpdatableService } from './updatable.service';
 
 @Injectable()
 export class GameLandscapeService extends UpdatableService {
   private readonly application = inject(ApplicationService);
   private readonly landscapes: BackgroundSprite[] = [];
 
-
   setup(): void {
     if (this.landscapes.length) {
       throw new Error('do not call setup twice');
     }
 
-    const background = new BackgroundSprite(
-      Texture.from('background'),
-      {
-        speedTilePositionY: 0.25,
-        speedTilePositionX: 0,
-        width: this.application.screen.width,
-        height: this.application.screen.height,
-      },
-    );
+    const background = new BackgroundSprite(Texture.from('background'), {
+      speedTilePositionY: 0.125,
+      speedTilePositionX: 0,
+      width: this.application.screen.width,
+      height: this.application.screen.height,
+    });
     this.landscapes.push(background);
     this.application.stage.addChild(background);
 
-    const cloud = new BackgroundSprite(
-      Texture.from('clouds'),
-      {
-        speedTilePositionY: 0,
-        speedTilePositionX: 0.25,
-        width: this.application.screen.width,
-        height: 103,
-        speedY: 0.75,
-        maxY: this.application.screen.height,
-      },
-    );
+    const cloud = new BackgroundSprite(Texture.from('clouds'), {
+      speedTilePositionY: 0,
+      speedTilePositionX: 0.025,
+      width: this.application.screen.width,
+      height: 103,
+      speedY: 0.075,
+      maxY: this.application.screen.height,
+    });
     cloud.y = Math.floor(this.application.screen.height / 2);
     this.landscapes.push(cloud);
     this.application.stage.addChild(cloud);
 
-    const cloud2 = new BackgroundSprite(
-      Texture.from('assets/game/clouds-transparent.png'),
-      {
-        speedTilePositionY: 0,
-        speedTilePositionX: 0.30,
-        width: this.application.screen.width,
-        height: 103,
-        speedY: 0.85,
-        maxY: this.application.screen.height,
-      },
-    );
+    const cloud2 = new BackgroundSprite(Texture.from('assets/game/clouds-transparent.png'), {
+      speedTilePositionY: 0,
+      speedTilePositionX: 0.013,
+      width: this.application.screen.width,
+      height: 103,
+      speedY: 0.085,
+      maxY: this.application.screen.height,
+    });
     cloud2.y = Math.floor(this.application.screen.height / 4);
     this.landscapes.push(cloud2);
     this.application.stage.addChild(cloud2);
   }
 
   update(ticker: Ticker): void {
-    this.landscapes.forEach(item => item.update(ticker));
+    this.landscapes.forEach((item) => item.update(ticker));
   }
 }
