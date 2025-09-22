304607b46aa1ee05f9e169d02ce7ecc63267acae
Thomas Renger <thomas.renger@gmail.com>
2025-09-22 21:16:31 +0200

    fix zoneless

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -45,11 +45,6 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js'; // Included with Angular CLI.
-
-(window as any).__Zone_disable_requestAnimationFrame = true; // disable patch requestAnimationFrame
-(window as any).__Zone_disable_on_property = true; // disable patch onProperty such as onclick
-(window as any).__zone_symbol__UNPATCHED_EVENTS = ['scroll', 'mousemove'];
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
